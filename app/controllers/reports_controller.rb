fe9c6c239797bbc25e666ea9dd6e5c80a5240169
ThiagoDuarteC <thiagodcorreia01@gmail.com>
2024-12-02 19:48:25 -0300

    Improve: adjust json response

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,19 +2,27 @@ class ReportsController < ApplicationController
   def category_by_debit
     @debit_category_totals = Transaction.where(user: current_user, deleted_at: nil, transaction_type: 'debit')
                                   .joins(:category)
-                                  .group('categories.id, categories.name')
+                                  .group('categories.name')
                                   .sum(:value)
 
-    render json: { categories: @debit_category_totals }
+    formatted_categories = @debit_category_totals.map do |name, value|
+      { name: name, value: value }
+    end
+
+    render json: { categories: formatted_categories }
   end
 
   def category_by_credit
     @credit_category_totals = Transaction.where(user: current_user, deleted_at: nil, transaction_type: 'credit')
                                   .joins(:category)
-                                  .group('categories.id, categories.name')
+                                  .group('categories.name')
                                   .sum(:value)
 
-    render json: { categories: @credit_category_totals }
+    formatted_categories = @credit_category_totals.map do |name, value|
+      { name: name, value: value }
+    end
+
+    render json: { categories: formatted_categories }
   end
 
   def balance_per_account
